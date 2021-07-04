eb0c356447c8b4433018258cf13414692b4a2ba1
Paweł Ciosek <ciosek.pawel@gmail.com>
2021-07-04 11:02:51 +0200

    portainer stack

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os, time, subprocess, re;
+
+print('\nReseting enviorment..\n')
+
+os.system('docker-compose up -d --force-recreate')
+
+print('\nWait for cointainers to warm up...\n')
+time.sleep(3)
+
+print('\nInit a docker swarm\n')
+
+r = subprocess.getoutput('docker exec manager docker swarm init')
+token = re.search(r'SW\S{1,}', r)[0]
+ip = re.search(r'[0-9]{3}\.\S{1,}', r)[0]
+
+print('join token: ' + token)
+print('IP manager: ' + ip)
+
+print('\nJoin all nodes\n')
+
+workersToJoin = ['worker1', 'worker2']
+for worker in workersToJoin:
+    command = 'docker exec %s docker swarm join --token %s %s' % (worker, token, ip)
+    r = subprocess.getoutput(command)
+    print('%s: %s' % (worker, r))
+
+print('\nShow swarm status\n')
+os.system('docker exec manager docker node ls')
+
+print('\nDeploy Portainer\n')
+os.system('docker exec manager docker stack deploy -c stack/portainer-agent-stack.yml portainer')
