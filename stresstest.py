bc24951a5e7f1bd70d4c1903ce0d2221e67360e6
Paweł Ciosek <ciosek.pawel@gmail.com>
2022-06-03 17:40:19 +0200

    stresser basic auth handler

--- stresstest.py
+++ stresstest.py
@@ -1,6 +1,21 @@
 import base64
+from random import random
 import time, sys
 import urllib.request
+
+def stresser(request):
+    name = "process_%s" % random()
+    while 1:
+        try:
+            start = time.time()
+            result = urllib.request.urlopen(request)
+            end = time.time()
+            print("process %s: %s" % (name, end - start))
+            time.sleep(interval)
+        except KeyboardInterrupt:
+            sys.exit()
+        except:
+            print('Connection problem!')
 
 address = sys.argv[1]
 try:
@@ -14,20 +29,11 @@ except IndexError:
     basicauth_username = ""
     basicauth_password = ""
 
-while 1:
-    try:
-        start = time.time()
-        request = urllib.request.Request(address)
-        if  basicauth:
-            data_string = "%s:%s" % (basicauth_username,basicauth_password)
-            data_bytes = data_string.encode("ascii")
-            b64auth = base64.b64encode(data_bytes)
-            request.add_header("Authorization", "Basic %s" % b64auth.decode('ascii'))
-        result = urllib.request.urlopen(request)
-        end = time.time()
-        print(end - start)
-        time.sleep(interval)
-    except KeyboardInterrupt:
-        sys.exit()
-    except:
-        print('Connection problem!')
+request = urllib.request.Request(address)
+if  basicauth:
+    data_string = "%s:%s" % (basicauth_username,basicauth_password)
+    data_bytes = data_string.encode("ascii")
+    b64auth = base64.b64encode(data_bytes)
+    request.add_header("Authorization", "Basic %s" % b64auth.decode('ascii'))
+    
+stresser(request)
