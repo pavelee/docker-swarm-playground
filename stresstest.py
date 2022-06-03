dec1063c25d9499629f67b61426c403c281357d3
Paweł Ciosek <ciosek.pawel@gmail.com>
2022-06-03 17:14:19 +0200

    stresser basic auth handler

--- stresstest.py
+++ stresstest.py
@@ -1,19 +1,29 @@
-import urllib.request, time, sys
+import base64
+import time, sys
+import urllib.request
 
 address = sys.argv[1]
 try:
     interval = float(sys.argv[2])
+    basicauth = sys.argv[3]
+    basicauth_username = sys.argv[4]
+    basicauth_password = sys.argv[5]
 except IndexError:
     interval = 0.1
-
-# print(interval)
-# exit()
+    basicauth = 0
+    basicauth_username = ""
+    basicauth_password = ""
 
 while 1:
     try:
         start = time.time()
-        fp = urllib.request.urlopen(address)
-        fp.close()
+        request = urllib.request.Request(address)
+        if  basicauth:
+            data_string = "%s:%s" % (basicauth_username,basicauth_password)
+            data_bytes = data_string.encode("ascii")
+            b64auth = base64.b64encode(data_bytes)
+            request.add_header("Authorization", "Basic %s" % b64auth.decode('ascii'))
+        result = urllib.request.urlopen(request)
         end = time.time()
         print(end - start)
         time.sleep(interval)
